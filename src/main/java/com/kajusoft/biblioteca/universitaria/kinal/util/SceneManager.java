03072ed4a658b4e72daee4467a2c9a6125a70ad2
CartoonXD364 <kevinajurod@gmail.com>
2026-09-09 17:02:12 -0600

    feature/implementacion del dashboard-view

--- src/main/java/com/kajusoft/biblioteca/universitaria/kinal/util/SceneManager.java
+++ src/main/java/com/kajusoft/biblioteca/universitaria/kinal/util/SceneManager.java
@@ -5,7 +5,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import main.java.com.kajusoft.biblioteca.universitaria.kinal.controller.DashboardController;
 import main.java.com.kajusoft.biblioteca.universitaria.kinal.controller.LoginController;
+import main.java.com.kajusoft.biblioteca.universitaria.kinal.dao.book.AuthorDAO;
+import main.java.com.kajusoft.biblioteca.universitaria.kinal.dao.book.BookDAO;
 import main.java.com.kajusoft.biblioteca.universitaria.kinal.dao.user.UserDAO;
 
 public class SceneManager {
@@ -26,6 +29,36 @@ public class SceneManager {
             if(clazz == LoginController.class){
                 UserDAO userDAO = new UserDAO();
                 return new LoginController(userDAO, this);
+            }
+            
+            try{
+                return clazz.getDeclaredConstructor().newInstance();
+            }catch(Exception e){
+                throw new RuntimeException("Error al generar el constructor");
+            }
+            
+        });
+        
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 600, 600);
+        stage.setMinHeight(500);
+        stage.setMinWidth(500);
+        stage.setScene(scene);  
+        stage.centerOnScreen();
+        stage.show();
+        
+    }
+    
+    public void showDashboardView()throws Exception{
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_PATH + "dashboard-view.fxml"));
+        
+        loader.setControllerFactory(
+        clazz -> {
+            if(clazz == DashboardController.class){
+                BookDAO bookDAO = new BookDAO();
+                AuthorDAO authorDAO = new AuthorDAO();
+                return new DashboardController(bookDAO, authorDAO, this);
             }
             
             try{
